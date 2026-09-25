4419218b9434d983315d6582542aea23c23b4fe8
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-25 06:10:46 -0600

    feat(models): Add model for entity categoria financiera

---
+++ src/models/categoriaFinanciera.js
@@ -0,0 +1,7 @@
+export class CategoriaFinanciera{
+    constructor(nombre, tipo, descripcion){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+    }
+}

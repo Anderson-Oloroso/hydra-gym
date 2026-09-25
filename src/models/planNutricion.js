1ab9213b37e6aca43f1306241cac2f3792c28f73
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-25 06:15:12 -0600

    feat(models): Add model for entity plan_nutricion

---
+++ src/models/planNutricion.js
@@ -0,0 +1,7 @@
+export class PlanNutricion{
+    constructor(id_cliente_plan, nombre, descripcion){
+        this.id_cliente_plan = id_cliente_plan;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+}

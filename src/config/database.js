4259d4b4eb0c9e85cc18f7bcc76de217879790a3
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-25 06:20:42 -0600

    Edit(connection): Modify conection and add functions to end Connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,34 @@
 import { createConnection } from 'mysql2/promise';
 import dotenv from 'dotenv';
+import chalk from 'chalk';
 
 dotenv.config();
 
+let dbConnection;
+
 export async function connection() {
-    let dbConnection;
-    
-    try{
-        dbConnection = await createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
-        });
-        console.log('Exito');
+  try {
+    if (dbConnection) {
+      return dbConnection;
     }
-    catch(err){
-        console.log('Error: ', err.message);
-    }
+
+    dbConnection = await createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+    console.log(chalk.green('The connection to MySQL was Successfull!'));
+
+    return dbConnection;
+  } catch (err) {
+    console.log(chalk.red(`Error: ${err.message}`));
+  }
 }
 
-connection();
+export async function closeConnection() {
+  if (dbConnection) {
+    await dbConnection.end();
+    console.log(chalk.yellow('Conection Closed'));
+  }
+}

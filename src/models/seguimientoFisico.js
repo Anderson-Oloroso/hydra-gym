a72f89249f9150c85c6091f80c507e63ad61ff96
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-25 06:16:16 -0600

    feat(models): Add model for entity seguimientoFisico

---
+++ src/models/seguimientoFisico.js
@@ -0,0 +1,11 @@
+export class SeguimientoFisico{
+    constructor(id_cliente_plan, semana, peso_kg, grasa_corporal, altura_cm, fotos, comentarios){
+        this.id_cliente_plan = id_cliente_plan;
+        this.semana = semana;
+        this.peso_kg = peso_kg;
+        this.grasa_corporal = grasa_corporal;
+        this.altura_cm = altura_cm;
+        this.fotos = fotos;
+        this.comentarios = comentarios;
+    }
+}

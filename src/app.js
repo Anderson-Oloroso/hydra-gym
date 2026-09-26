b95be482c95e88ab951716370dfe420aa4f08672
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-25 21:43:47 -0600

    feat(cliente): Add function list of clientes to menu

--- src/app.js
+++ src/app.js
@@ -1,32 +1,57 @@
-import { mainMenu, submenu } from './utils/menu.js';
+import { mainMenu, submenu, pause, clearScreen } from './utils/menu.js';
+import { listClient } from './commands/clienteCmd.js';
+import { closeConnection } from './config/database.js';
 import chalk from 'chalk';
 
 async function main(){
     while(true){
         try{
-            console.clear();
+            clearScreen();
             const selectedEntity = await mainMenu();
 
             if(selectedEntity === 'SALIR'){
                 console.log(chalk.green.bold('Saliendo del programa ...'));
-                return;
+                await closeConnection();
+                process.exit(0);
             }
 
             while(true){
+                clearScreen();
                 const selectedAction = await submenu(selectedEntity);
 
                 if(selectedAction === 'Regresar'){
                     break;
                 }
 
-                console.log(chalk.cyan.bold(`Ejecutando: [${selectedAction}] en [${selectedEntity}]`));
+                console.log(chalk.cyan.bold(`\nEjecutando: [${selectedAction}] en [${selectedEntity}]...\n`));
 
-                await new Promise((resolve) => setTimeout(resolve, 1500));
+                switch (selectedEntity) {
+                    case 'CLIENTES':
+                        switch (selectedAction) {
+                            case 'Listar registros':
+                                await listClient();
+                                break;
+
+                            default:
+                                console.log(chalk.yellow(`Acción no implementada: ${selectedAction}`));
+                                break;
+                        }
+                        break;
+
+                    default:
+                        console.log(chalk.yellow(`Entidad no implementada: ${selectedEntity}`));
+                        break;
+                }
+
+                await pause();
             }
         }
         catch(err){
-            console.log('Error: ',err.message);
-            break;
+            if (err) {
+                console.log(chalk.red('Error: '), err.message || err);
+            }
+            await closeConnection();
+            process.exit(0);
         }
     }
 }

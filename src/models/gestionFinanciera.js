0bdfbafccda81b9373686574717bf858dd1eaf0c
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-25 21:11:34 -0600

    Edit(model): Edit model gestionFinanciera

--- src/models/gestionFinanciera.js
+++ src/models/gestionFinanciera.js
@@ -1,9 +1,8 @@
 export class GestionFinanciera{
-    constructor(id_categoria, id_cliente, monto, fecha_transaccion, descripcion){
+    constructor(id_categoria, id_cliente, monto, descripcion){
         this.id_categoria = id_categoria;
         this.id_cliente = id_cliente;
         this.monto = monto;
-        this.fecha_transaccion = fecha_transaccion;
         this.descripcion = descripcion;
     }
 }
